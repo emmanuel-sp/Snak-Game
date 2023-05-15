4e27f2560f71177b322fa0a250027ae8d1351d1f
emmanuel-sp <esp48321@uga.edu>
2023-05-15 13:55:11 -0400

    Added stack to track body, still no growing feature

--- src/main/java/happy/test/javafxte/HelloApplication.java
+++ src/main/java/happy/test/javafxte/HelloApplication.java
@@ -25,6 +25,7 @@ import javafx.animation.Timeline;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.util.Duration;
+import java.util.Stack;
 import java.util.concurrent.TimeUnit;
 
 public class HelloApplication extends Application {
@@ -41,22 +42,17 @@ public class HelloApplication extends Application {
     private Rectangle seed;
 
     //Variables
+    private static Stack<Rectangle> body = new Stack<>();
     private final static int ROWS = 19;
     private final static int COLS = 19;
     private static int[] headA = new int[]{ROWS / 2, COLS / 2};
     private static int[] tailA = new int[]{ROWS / 2, COLS / 2};
 
-
-
+    private boolean going = false;
     private boolean goingUp = false;
     private boolean goingDown = false;
     private boolean goingRight = false;
     private boolean goingLeft = false;
-
-    private boolean wentLeft = false;
-    private boolean wentRight = false;
-    private boolean wentUp = false;
-    private boolean wentDown = false;
 
     private static int moved = 0;
     private static int size = 1;
@@ -79,24 +75,39 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
         scene.setOnKeyPressed(e -> {
+            if (!going) {
+                runInNewThread(() -> go(), "");
+            }
             if (e.getCode() == KeyCode.UP) {
+                going = true;
                 if (!goingUp && !goingDown) {
-                    runInNewThread(() -> this.goUp(), "Up");
+                    goingUp = true;
+                    goingRight = false;
+                    goingLeft = false;
                 }
             }
             if (e.getCode() == KeyCode.DOWN) {
+                going = true;
                 if (!goingDown && !goingUp) {
-                    runInNewThread(() -> this.goDown(), "Down");
+                    goingDown = true;
+                    goingRight = false;
+                    goingLeft = false;
                 }
             }
             if (e.getCode() == KeyCode.RIGHT) {
+                going = true;
                 if (!goingRight && !goingLeft) {
-                    runInNewThread(() -> this.goRight(), "Right");
+                    goingRight = true;
+                    goingUp = false;
+                    goingDown = false;
                 }
             }
             if (e.getCode() == KeyCode.LEFT) {
+                going = true;
                 if (!goingLeft && !goingRight) {
-                    runInNewThread(() -> this.goLeft(), "Left");
+                    goingLeft = true;
+                    goingUp = false;
+                    goingDown = false;
                 }
             }
         });
@@ -116,14 +127,13 @@ public class HelloApplication extends Application {
         recs[ROWS / 2][COLS / 2].setFill(Color.GREEN);
         head = recs[ROWS / 2][COLS / 2];
         tail = recs[ROWS / 2][COLS / 2];
+        body.push(head);
         seed = recs[5][7];
         seed.setFill((Color.RED));
 
 
         root.setAlignment(Pos.CENTER);
         root.setPadding(new Insets(10,10,10,10));
-        click.setOnAction((e) -> onHelloButtonClick());
-
 
     }
 
@@ -132,133 +142,74 @@ public class HelloApplication extends Application {
         launch();
     }
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome!");
-    }
+    public void go() {
+        try {
+            while (going) {
+                for (Rectangle item : body) {
+                    item.setFill(Color.BLACK);
+                }
+                body.pop();
+                if (goingUp) {
+                    headA[0]--;
+                    head = recs[headA[0]][headA[1]];
+                }
+                if (goingDown) {
+                    headA[0]++;
+                    head = recs[headA[0]][headA[1]];
+                }
+                if (goingRight) {
+                    headA[1]++;
+                    head = recs[headA[0]][headA[1]];
+                }
+                if (goingLeft) {
+                    headA[1]--;
+                    head = recs[headA[0]][headA[1]];
+                }
+                body.push(head);
+                for (Rectangle item : body) {
+                    item.setFill(Color.GREEN);
+                }
+                Thread.sleep(75);
 
-    public void goUp() {
-        if (head == seed) {
-            size++;
-        }
-        //goingUp = !goingUp;
-        //goingDown = false;
-        //goingLeft = false;
-        //goingRight = false;
-       // while(goingUp) {
-            try {
-                tail.setFill(Color.BLACK);
-                tailA[0]--;
-                tail = recs[tailA[0]][tailA[1]];
-                headA[0]--;
-                head = recs[headA[0]][headA[1]];
-                moved++;
-            } catch (IndexOutOfBoundsException e) {
-                goingUp = false;
+            }
+        } catch (IndexOutOfBoundsException | InterruptedException e) {
+            going = false;
+            System.out.println("ERROR");
+            if (goingUp) {
+                goDown();
+            }
+            if (goingDown) {
+                goUp();
+            }
+            if (goingRight) {
+                goLeft();
+            }
+            if (goingLeft) {
+                goRight();
             }
             head.setFill(Color.GREEN);
-            wentUp = true;
-
-            //           try {
-            //             Thread.sleep(150);
-            //       } catch (InterruptedException ex) {
-            //         throw new RuntimeException(ex);
-            //   }
         }
-  //  }
-    public void goDown() {
-        if (head == seed) {
-            size++;
-        }
-        //goingDown = !goingDown;
-        //goingUp = false;
-        //goingLeft = false;
-        //goingRight = false;
-      //  while(goingDown) {
-            try {
-                tail.setFill(Color.BLACK);
-                tailA[0]++;
-                tail = recs[tailA[0]][tailA[1]];
-                headA[0]++;
-                head = recs[headA[0]][headA[1]];
-                moved++;
-            } catch (IndexOutOfBoundsException e) {
-                goingDown = false;
-            }
-            head.setFill(Color.GREEN);
-            wentDown = true;
-
- //           try {
-   //             Thread.sleep(150);
-     //       } catch (InterruptedException ex) {
-       //         throw new RuntimeException(ex);
-         //   }
-    //    }
-    }
-
-    public void goRight() {
-        if (head == seed) {
-            size++;
-        }
-       // goingRight = !goingRight;
-        //goingDown = false;
-        //goingLeft = false;
-        //goingUp = false;
-     //   while(goingRight) {
-            try {
-                tail.setFill(Color.BLACK);
-                tailA[1]++;
-                tail = recs[tailA[0]][tailA[1]];
-                headA[1]++;
-                head = recs[headA[0]][headA[1]];
-                moved++;
-            } catch (IndexOutOfBoundsException e) {
-                goingRight = false;
-            }
-            head.setFill(Color.GREEN);
-            wentRight = true;
-
-        //           try {
-        //             Thread.sleep(150);
-        //       } catch (InterruptedException ex) {
-        //         throw new RuntimeException(ex);
-        //   }
-      //  }
-    }
-
-    public void goLeft() {
-        //goingLeft = !goingLeft;
-        //goingDown = false;
-        //goingUp = false;
-        //goingRight = false;
-      //  while(goingLeft) {
-            if (head == seed) {
-                size++;
-            }
-            try {
-                tail.setFill(Color.BLACK);
-                tailA[1]--;
-                tail = recs[tailA[0]][tailA[1]];
-                headA[1]--;
-                head = recs[headA[0]][headA[1]];
-                moved++;
-            } catch (IndexOutOfBoundsException e) {
-                goingLeft = false;
-            }
-            head.setFill(Color.GREEN);
-            wentLeft = true;
-
-        //           try {
-        //             Thread.sleep(150);
-        //       } catch (InterruptedException ex) {
-        //         throw new RuntimeException(ex);
-        //   }
-      //  }
-    }
-
-    private void grow() {
 
     }
+
+    private void goUp() {
+        headA[0]--;
+        head = recs[headA[0]][headA[1]];
+    }
+    private void goDown() {
+        headA[0]++;
+        head = recs[headA[0]][headA[1]];
+
+    }
+    private void goRight() {
+        headA[1]++;
+        head = recs[headA[0]][headA[1]];
+    }
+    private void goLeft() {
+        headA[1]--;
+        head = recs[headA[0]][headA[1]];
+    }
+
     public static void runInNewThread(Runnable task, String name) {
         Thread t = new Thread(task, name);
         t.setDaemon(true);
